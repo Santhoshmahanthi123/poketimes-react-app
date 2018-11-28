3fcc830387546394a6db93b23d2581c42eaa40df
santhosh <singumahanthisanthosh@gmail.com>
2018-11-29 05:28:42 +0530

    a tag changed to link tag to prevent reloading of dom

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,18 @@
 import React from 'react';
+//link,Navlink used to request to components that prevent dom to reload
+//always and not to send requests always to server
+import { Link, NavLink} from 'react-router-dom';
 //UI baseed component which is a function it is Navbar component
 const Navbar = () =>{
+    //NavLink is used to apply some stylink to components in future
     return(
       <nav className="nav-wrapper red darken-3">
           <div className="container">
               <a className="brand-logo">Poke'Times</a>
               <ul className="right">
-                  <li><a href="/">Home</a></li>
-                  <li><a href="/about">About</a></li>
-                  <li><a href="/contact">Contact</a></li>
+                  <li><Link to="/">Home</Link></li>
+                  <li><NavLink to="/about">About</NavLink></li>
+                  <li><Link to="/contact">Contact</Link></li>
               </ul>
           </div>
       </nav> 
