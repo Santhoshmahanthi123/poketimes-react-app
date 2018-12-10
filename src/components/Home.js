9275a3eac854cff555905113e71d3863b2c8759b
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2018-12-11 04:28:16 +0530

    using images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import axios from 'axios'
 import {Link} from 'react-router-dom'
+import Pokeball from '../pokeball.png'
 //UI baseed component which is a function it is Home component
 //convert fun to class since we use axios to make it a life cycle hook
 class Home extends Component{
@@ -26,9 +27,10 @@ class Home extends Component{
            posts.map(post =>{
                return (
                    <div className="post card" key={post.id}>
+                       <img src={Pokeball} alt="A pokeball"/>
                        <div className="card-content">
                            <Link to={'/' +post.id }>
-                           <span className="card-title">{post.title}</span>
+                           <span className="card-title red-text">{post.title}</span>
                            </Link>
                            <p>{post.body}</p>
                        </div>
@@ -40,7 +42,7 @@ class Home extends Component{
            <div className="center">No posts yet!</div>
        )
     return(
-        <div className="container">
+        <div className="container home">
             <h4 className="center">Home</h4>
             {postList}
         </div>
