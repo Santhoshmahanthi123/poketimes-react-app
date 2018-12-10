07bd0a3554d32b723e43f1ba2b1a08662d2fbf4c
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2018-12-11 03:09:23 +0530

    using axios for displaying posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React,{Component} from 'react';
+import axios from 'axios'
 //UI baseed component which is a function it is Home component
-const Home = () =>{
+//convert fun to class since we use axios to make it a life cycle hook
+class Home extends Component{
+    state = {
+        posts : []
+    }
+    //method to use axios
+    componentDidMount(){
+
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res =>{
+            console.log(res);
+            this.setState({
+                //for adding first 10 data items to state posts
+                posts : res.data.slice(0,10)
+            })
+        })
+    }
+   render(){
+       const {posts} = this.state;
+       //to cycle through each post
+       const postList = posts.length ? (
+           posts.map(post =>{
+               return (
+                   <div className="post card" key={post.id}>
+                       <div className="card-content">
+                           <span className="card-title">{post.title}</span>
+                           <p>{post.body}</p>
+                       </div>
+                   </div>
+               )
+
+           })
+       ) : (
+           <div className="center">No posts yet!</div>
+       )
     return(
         <div className="container">
             <h4 className="center">Home</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eligendi delectus autem quasi labore asperiores quod architecto, alias repudiandae illum, sint dolorem veritatis placeat vitae. Itaque est expedita eaque quae?</p>
+            {postList}
         </div>
-    );
+    )
+   }
 }
 export default Home;
